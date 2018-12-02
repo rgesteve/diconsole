cee885b963c1baffe70d2f75693d017eb21cba9b
R. G. Esteves <rgesteves@gmail.com>
2018-12-01 23:33:30 -0800

    Initial import

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
