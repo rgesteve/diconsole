60acfd9c9fb3e889312626bf6e6fedfccf9da5b8
R. G. Esteves <rgesteves@gmail.com>
2018-12-02 13:53:19 -0800

    Try at parsing elements from JSON

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
 
 namespace diconsole
 {
@@ -6,7 +9,29 @@ namespace diconsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+	    Console.WriteLine("Populating databases...");
+
+	    // Read JSON directly from a file
+	    JObject data = JObject.Parse(File.ReadAllText(@"data.json"));
+	    JToken processId = (JToken)data.SelectToken("processId");
+	    JToken capabilities = (JToken)data.SelectToken("capabilities");
+
+	    Console.WriteLine($"testing: {processId}, [{processId.GetType()}]");
+	    Console.WriteLine($"testing: {capabilities}, [{capabilities.GetType()}]");
+	    Test(processId);
+	    Test(capabilities);
+	    
+            Console.WriteLine("Done!");
+	    Environment.Exit(0);
         }
+
+	static void Test(JToken token)
+	{
+	    if (token.Type == JTokenType.Object) {
+	      Console.WriteLine("This is an object...");
+	    } else {
+    	      Console.WriteLine("Can't tell what this is");
+	    }
+	}
     }
 }
