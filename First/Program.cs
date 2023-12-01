73f95ebc4301951c2a0ed44aabebfa1d3a244eb3
R. G. Esteves <rgesteves@gmail.com>
2023-12-01 02:58:23 -0800

    formatting changes

--- First/Program.cs
+++ First/Program.cs
@@ -10,16 +10,39 @@ namespace First;
 
 class Program
 {
-    static void Main(string[] args)
+    static /* void */ async Task Main(string[] args)
     {
-      Host.CreateDefaultBuilder(args)
-      .ConfigureLogging((_, configLogging) => {
-        //configLogging.ClearProviders();
-      })
-      .ConfigureServices(services => {
-        services.AddHttpClient();
-      })
-      .Build().Run();
-      Console.WriteLine("Hello, World!");
+        var host = Host.CreateDefaultBuilder(args)
+        .ConfigureLogging((_, configLogging) =>
+        {
+            //configLogging.ClearProviders();
+        })
+        .ConfigureServices(services =>
+        {
+            services.AddHttpClient();
+        })
+        .Build();
+        
+        /* using */ var httpClientFactory = host.Services.GetService<IHttpClientFactory>(); // not sure if this should be a disposable (in which case I could uncomment the `using` stmt)
+        if (httpClientFactory == null) {
+            Console.WriteLine("Error getting http factory");
+        } else {
+            var httpClient = httpClientFactory.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://google.com");
+            using var response = await httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Successfully connected to Google.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to Google: " + response.StatusCode);
+            }
+        }
+
+        // host.Run(); // this goes into an infinite loop that has to be broken out using Ctrl-C
+        Console.WriteLine("Hello, World!");
     }
 }
