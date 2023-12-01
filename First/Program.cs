e54a5a28d965831b5c9feece4e700c62fc6fe739
R. G. Esteves <rgesteves@gmail.com>
2023-11-30 22:50:05 -0800

    Bare-bones DI-using console app

---
+++ First/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
+
+namespace First;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Host.CreateDefaultBuilder(args)
+      .ConfigureLogging((_, configLogging) => {
+        // configLogging.ClearProviders();
+      }).Build().Run();
+      Console.WriteLine("Hello, World!");
+    }
+}
